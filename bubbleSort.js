0d58a6d1847bc8ced6d75e83da538d78c13e68dc
Devishanmugam18 <79523505+Devishanmugam18@users.noreply.github.com>
2023-08-01 23:29:18 +0530

    Create bubbleSort.js
    Input : [23, 43, 12, 56, 35]
    Output : [ 12, 23, 35, 43, 56 ]
    
    Time Complexity : O(N^2)

---
+++ bubbleSort.js
@@ -0,0 +1,33 @@
+function bubbleSort(array){
+    let flag = false;
+    for(var i = 0; i <= array.length-1; i++){
+        flag = false;
+        for(var j = 0; j < ( array.length - i -1); j++){
+
+            // Comparing two adjacent numbers 
+            // and see if first is greater than second
+            if(array[j] > array[j+1]){
+
+            // Swap them if the condition is true 
+            var temp = array[j]
+            array[j] = array[j + 1]
+            array[j+1] = temp
+            flag = true;
+            }
+        }
+
+        if(!flag){
+            console.log(array)
+            return;
+        }
+}
+
+console.log(array)
+}
+
+
+var array = [23, 43, 12, 56, 35];
+
+// calling the bubbleSort Function
+bubbleSort(array)
+
